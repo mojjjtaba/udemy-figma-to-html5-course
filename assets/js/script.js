59fcdca7241f3e27c7a4d865f292eb59d968cf30
Mojtaba <smo.hayati@gmail.com>
2022-06-18 17:40:34 +0430

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,22 @@ navLinks.forEach((item) => {
 document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', function(){
     document.querySelector('#sidebar').classList.toggle('open')
 })
+
+
+
+var options = {
+  strings: ['Front-End web developer', 'Back-End web developer', 'Web designer'],
+  typeSpeed: 70,
+  backSpeed: 10,
+  loop: true
+};
+
+var typed = new Typed('.field h2', options);
+
+
+
+for(let i = 1; i <= 15; i++) {
+    let meteor = document.createElement('span');
+    meteor.className = 'meteor';
+    document.querySelector('#home .meteor-shower').append(meteor)
+}
