aa0296a0d0e6b2aa1b72d72a7438b2a0692d43ca
Mojtaba <smo.hayati@gmail.com>
2022-06-28 18:17:45 +0430

    completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let navLinks = document.querySelectorAll('a.inner-link');
 
 navLinks.forEach((item) => {
-    item.addEventListener('click', function(){
+    item.addEventListener('click', function () {
         console.log(item)
         document.querySelector('nav ul li a.active').classList.remove('active')
         document.querySelector(`nav ul li a[href='${item.getAttribute('href')}']`).classList.add('active')
@@ -12,7 +12,7 @@ navLinks.forEach((item) => {
 
 
 
-document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', function(){
+document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', function () {
     document.querySelector('#sidebar').classList.toggle('open')
 })
 
@@ -23,14 +23,38 @@ var options = {
     loop: true,
     typeSpeed: 70,
     backSpeed: 10
-  };
-  
+};
+
 new Typed('.field h2', options);
 
 
 
-for(let i = 1; i <= 15; i++) {
+for (let i = 1; i <= 15; i++) {
     let meteor = document.createElement('span');
     meteor.classList = 'meteor'
     document.querySelector('#home .meteor-shower').append(meteor);
+}
+
+
+
+const shuffleInstance = new Shuffle(document.querySelector('#my_work .work-items'), {
+    itemSelector: '.item'
+});
+
+const filterButtons = document.querySelectorAll('#my_work .filters button')
+
+filterButtons.forEach((item) => {
+    item.addEventListener('click', workFilter)
+})
+
+
+function workFilter() {
+    const clickedButton = event.currentTarget;
+    const clickedButtonGroup = clickedButton.getAttribute('data-group');
+    const activeButton = document.querySelector('#my_work .filters button.active');
+
+    activeButton.classList.remove('active');
+    clickedButton.classList.add("active");
+
+    shuffleInstance.filter(clickedButtonGroup)
 }
