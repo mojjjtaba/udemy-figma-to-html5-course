d9389967e882a7aeb867a750942010522fbaafcf
Mojtaba <smo.hayati@gmail.com>
2022-07-02 16:19:13 +0430

    completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,3 +86,22 @@ workModalElement.addEventListener('hide.bs.modal', function (event) {
     document.getElementById('my_work').classList.remove('blur');
     document.getElementById('sidebar').classList.remove('blur');
 })
+
+
+
+
+
+let testimonialImages = document.querySelectorAll('#testimonial .images img');
+
+testimonialImages.forEach((item, index) => {
+    let position = index + 1;
+    
+    item.addEventListener('click', function() {
+        document.querySelector('#testimonial .images img.active').classList.remove('active')
+        document.querySelector(`#testimonial .images img:nth-child(${position})`).classList.add('active')
+
+        document.querySelector('#testimonial .comments .item.active').classList.remove('active')
+        document.querySelector(`#testimonial .comments .item:nth-child(${position})`).classList.add('active')
+    })
+})
+
